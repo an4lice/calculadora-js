7e00a40bd014b107893c63febffa47f946c6a8aa
sumaya <su_analice@hotmail.com>
2021-08-26 19:23:33 -0300

    Refactoring of variables.

--- script.js
+++ script.js
@@ -3,36 +3,28 @@ var num1 = 10
 var num2 = 5
 
 function calcSomar(num1, num2){
-    var soma1 = num1
-    var soma2 = num2
-    var resSoma = soma1 + soma2
+    var resSoma = num1 + num2
     return resSoma
 }
 
 calcSomar(10, 5)
 
 function calcSub(num1, num2){
-    var sub1 = num1
-    var sub2 = num2
-    var resSub = sub1 - sub2
+    var resSub = num1 - num2
     return resSub
 }
 
 calcSub (10, 5)
 
 function calcDiv(num1, num2){
-    var div1 = num1
-    var div2 = num2
-    var resDiv = div1 / div2
+    var resDiv = num1 / num2
     return resDiv
 }
 
 calcDiv(10, 5)
 
 function calcMult(num1, num2){
-    var mult1 = num1
-    var mult2 = num2
-    var resMult = mult1 * mult2
+    var resMult = num1 * num2
     return resMult
 }
 
