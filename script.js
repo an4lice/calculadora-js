17411cec8789b15defd1c7eab09fcaeca832a4ef
sumaya <su_analice@hotmail.com>
2021-08-26 19:05:29 -0300

    Inclusion of parameters in operating functions.

--- script.js
+++ script.js
@@ -2,30 +2,38 @@
 var num1 = 10
 var num2 = 5
 
-function calcSomar(){
+function calcSomar(num1, num2){
     var soma1 = num1
     var soma2 = num2
     var resSoma = soma1 + soma2
     return resSoma
 }
 
-function calcSub(){
+calcSomar(10, 5)
+
+function calcSub(num1, num2){
     var sub1 = num1
     var sub2 = num2
     var resSub = sub1 - sub2
     return resSub
 }
 
-function calcDiv(){
+calcSub (10, 5)
+
+function calcDiv(num1, num2){
     var div1 = num1
     var div2 = num2
     var resDiv = div1 / div2
     return resDiv
 }
 
-function calcMult(){
+calcDiv(10, 5)
+
+function calcMult(num1, num2){
     var mult1 = num1
     var mult2 = num2
     var resMult = mult1 * mult2
     return resMult
 }
+
+calcMult(10, 5)
