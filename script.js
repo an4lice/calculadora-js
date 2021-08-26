85a3221b5622f80efc716afea3bf5de631f7cf62
sumaya <su_analice@hotmail.com>
2021-08-26 18:53:03 -0300

    Simple math operations done

---
+++ script.js
@@ -0,0 +1,31 @@
+
+var num1 = 10
+var num2 = 5
+
+function calcSomar(){
+    var soma1 = num1
+    var soma2 = num2
+    var resSoma = soma1 + soma2
+    return resSoma
+}
+
+function calcSub(){
+    var sub1 = num1
+    var sub2 = num2
+    var resSub = sub1 - sub2
+    return resSub
+}
+
+function calcDiv(){
+    var div1 = num1
+    var div2 = num2
+    var resDiv = div1 / div2
+    return resDiv
+}
+
+function calcMult(){
+    var mult1 = num1
+    var mult2 = num2
+    var resMult = mult1 * mult2
+    return resMult
+}
